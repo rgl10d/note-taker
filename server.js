c3b038cd4de73598d830a803a357b4c6fd7649fe
Garrett Lee <rgl10d@gmail.com>
2020-11-17 14:12:52 -0500

    added path npm and html view routes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
+const fs = require("fs");
 const express = require("express");
-// const path = require("path");
+const path = require("path");
 
 const app = express();
 
@@ -8,11 +9,24 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
+// View routes
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "notes.html"));
+});
+
+
+// API routes
 app.get("/api/config", (req, res) => {
     res.json({
         success: "true"
     });
 });
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
