6a75bd15aca7436d7c897291c7034f8d17bde70d
Garrett Lee <rgl10d@gmail.com>
2020-11-18 22:45:47 -0500

    completed post function, added random id function and some of the readme

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# note-taker
+# My Note Taker [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Description 
+
+My Note Taker is a simple text saving app.
+
+Technologies used:
+- JavaScript
+- NodeJS
+- ExpressJS
+- Modules
+- Herokuapp
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Questions](#questions)
+* [License](#license)
+
+## Installation
+Nothing needs to be installed! Just head over to:
+
+## Usage 
+
+DEMO VIDEO: 
+
+Simply type in your notes section title and the notes themselves on the right portion of the screen and hit the save button at the top right when you're finished to save them.
+
+## Questions?
+
+Visit me at my GitHub page: https://www.github.com/rgl10d
+
+Or email me: rgl10d@gmail.com
+
+## License
+
+MIT License
+
+Copyright © [2020] [Garrett Lee]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- server.js
+++ server.js
@@ -1,46 +1,58 @@
+// REQUIREMENTS
 const fs = require("fs");
 const express = require("express");
 const path = require("path");
-
+// EXPRESS INSTANCE CREATION
 const app = express();
-
+// PORT DECLARATION
 const PORT = process.env.PORT || 8080;
-
+// NOTES DATABASE FILE PATH
 const dbFile = "./db/db.json";
 
-// Middleware
+// MIDDLEWARE
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Middleware for statuc file connection (The CSS)
+// MIDDLEWARE for static file connection (The CSS in this case)
 app.use(express.static(path.join(__dirname, 'public')));
+
+// RANDOM ID FUNCTION
+function guidGenerator() {
+    let S4 = function() {
+       return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+    };
+    return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+};
 
 // VIEW ROUTES
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
-// "*" will default to home/root if there is no matching route found
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 // API ROUTES
+// GET NOTES
 app.get("/api/notes", (req, res) => {
     // Should read the `db.json` file and return all saved notes as JSON.
     fs.readFile(dbFile, (err, data) => {
         if (err) throw err;
         const savedNotes = JSON.parse(data);
+        console.log(savedNotes);
         return res.json(savedNotes);
       });
 });
-
-app.post("/api/notes", (req, res) => {
-    // Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
-
-})
-
+// POST NEW NOTE
+app.post('/api/notes', (req, res) => {
+    const newNotes = JSON.parse(fs.readFileSync(dbFile));
+    newNotes.id = guidGenerator();
+    newNotes.push(req.body);
+    fs.writeFileSync(dbFile, JSON.stringify(newNotes));
+    return res.json(newNotes);
+});
+// DELETE A NOTE
 app.delete("/api/notes/:id", (req, res) => {
     // Should receive a query parameter containing the id of a note to delete.
-    // This means you'll need to find a way to give each note a unique id when it's saved. 
     // In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
 })
 
